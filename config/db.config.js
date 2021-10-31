a66802cbba8ad2e72ee8f9594e08149c0e650043
AfroBonsai <geozam89@gmail.com>
2021-10-31 20:33:08 +0100

    Creación archivos necesarios

---
+++ config/db.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  url: "mongodb://root5:pass@192.168.1.55:27017/movies_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" //Cadena de conexión mongo db
+};

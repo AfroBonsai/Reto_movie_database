7db03b6e64070d619a7ad2d495cd233a366b0063
AfroBonsai <geozam89@gmail.com>
2021-11-01 15:57:03 +0100

    Routes hechos, atributos peliculas hechos required

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middlewares/auth.js');
 
-//Importo Controllers
-const movies = require("../controllers/movie.controller");
+//Import Controllers
+const users = require("../controllers/user.controller");
   
-router.post("/", movies.create); // Create a new movie
-router.get("/", movies.findAll); // Retrieve all movies
-router.get("/available", movies.findAllAvailable); // Retrieve all available movies
-router.get("/:id", movies.findOne); // Retrieve a single movie with id
-router.put("/:id", movies.update); // Update a movie with id
-router.delete("/:id", movies.delete); // Delete a movie with id
-router.delete("/", movies.deleteAll); // Create a new movie
+// router.post("/", movies.create); // Create a new movie
+// router.get("/", movies.findAll); // Retrieve all movies
+// router.get("/available", movies.findAllAvailable); // Retrieve all available movies
+// router.get("/:id", movies.findOne); // Retrieve a single movie with id
+// router.put("/:id", movies.update); // Update a movie with id
+// router.delete("/:id", movies.delete); // Delete a movie with id
+// router.delete("/", movies.deleteAll); // Create a new movie
+
+
+router.post("/login", users.logIn); // Login as an user
+router.post('/register', auth, users.register); // Create a new user - *Requires Admin privileges*
+
+router.get("/:id", users.findById); // Show user information
+router.get("/", users.findAll); // Show all users
+
+router.put("/:id", users.update); // Update user information
+
+router.delete("/:id", auth, users.delete); // Delete an user - *Requires Admin privileges*
+router.delete("/", auth, users.deleteAll); // -WARNING- Delete ALL users! - *Requires Admin privileges*
   
 
-module.exports = router;
+module.exports = users;

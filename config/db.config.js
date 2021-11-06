73b46a2d2088e7d8b98f65efbb55cf30f60404d4
AfroBonsai <geozam89@gmail.com>
2021-11-06 13:35:55 +0100

    prueba errores db config y heroku

--- config/db.config.js
+++ config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   // url: "mongodb://root5:pass@192.168.1.55:27017/movies_db?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false" //Cadena de conexión mongo db
 
 
-url: "mongodb+srv://George:G60183Trex**@notflix.f5w6h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// url: "mongodb+srv://George:G60183Trex**@notflix.f5w6h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 };
 
 /*

59f0477e39e1d57cc80cb38fc10a14fd9030bcc4
AfroBonsai <geozam89@gmail.com>
2021-11-21 19:19:23 +0100

    pruebas coenxion

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const logger = require('./config/winston');
 const cors = require("cors"); // Import cors module
 const db = require("./models");
 const router = require('./router.js');
-const IP = require("../Reto_movie_database/config/db.config");
-// import { IP as _IP } from "../Reto_movie_database/config/db.config"; //experimento ES6
+// const IP = require("../Reto_movie_database/config/db.config");
 
 const app = express();
-const PORT = process.env.PORT || 3000; //Configuramos puerto heroku
+const PORT = process.env.PORT || 4000; //Configuramos puerto heroku
 
 //Config Cors Options
 var corsOptions = {
@@ -25,7 +24,7 @@ app.use(express.json());
 app.use(cors(corsOptions)); //Add CORS Middleware
 
 //Rutas
-app.get('/', (req, res) => {res.send("Welcome to AfroBonsai's movie database.");});
+app.get('/', (req, res) => {res.send("Welcome to Notflix database.");});
 app.use(router);
 
 db.mongoose
@@ -35,8 +34,8 @@ db.mongoose
   })
   .then(() => {
     console.log("Connected to the database!");
-    // app.listen(PORT, ()=> console.log(` < Connected to local host on port ${PORT} > `.bgGreen.black));
-    app.listen(PORT, ()=> console.log(` < Connected to local host `.bgGreen.black + `${IP.IP}`.bgBrightCyan.black + ` on port `.bgGreen.black + `${PORT}`.bgBrightCyan.black + ` > `.bgGreen.black));
+    app.listen(PORT, ()=> console.log(` < Connected to local host on port ${PORT} > `.bgGreen.black));
+    // app.listen(PORT, ()=> console.log(` < Connected to local host `.bgGreen.black + `${IP.IP}`.bgBrightCyan.black + ` on port `.bgGreen.black + `${PORT}`.bgBrightCyan.black + ` > `.bgGreen.black));
   })
   .catch(err => {
     console.log("Cannot connect to the database!", err);
